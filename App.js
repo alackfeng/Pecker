872e2ea19184322c541022de3db1a21a826eefe8
tokenfun <tokenfun@tokenfundeMBP.lan>
2018-11-02 10:36:05 +0800

    add eosjs support

--- App.js
+++ App.js
@@ -9,6 +9,16 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'eosjs';
+
+const defaultPrivateKey = "5JbuZx3MFvUY1FkYoWdR1o8b4TRCZwQJQzCvrT6JwVdQgLzscVo"; // useraaaaaaaa
+const signatureProvider = new JsSignatureProvider([defaultPrivateKey, '5J4j8MJdhiHGmJ26CGTHvqMWYeYNMTEL6itTaxXFPAUpPfPm61L']);
+
+const rpc = new JsonRpc('http://127.0.0.1:8000', { fetch });
+
+const api = new Api({ rpc, signatureProvider, /*textDecoder: new TextDecoder(), textEncoder: new TextEncoder()*/ });
+
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -18,11 +28,74 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      info: null,
+      info1: null,
+    }
+
+  }
+
+  componentDidMount() {
+    (async (_This) => {
+      try {
+
+      const result = await api.transact({
+        actions: [{
+          account: 'eosio.token',
+          name: 'transfer',
+          authorization: [{
+            actor: 'taurus',
+            permission: 'active',
+          }],
+          data: {
+            from: 'taurus',
+            to: 'alackfeng521',
+            quantity: '0.0001 EOS',
+            memo: '',
+          },
+        }]
+      }, {
+        blocksBehind: 3,
+        expireSeconds: 30,
+      });
+      console.dir(result);
+      _This.setState({info: result.transaction_id});
+
+      } catch (e) {
+        console.log('\nCaught exception: ' + e);
+        if (e instanceof RpcError)
+          console.log(JSON.stringify(e.json, null, 2));
+        _This.setState({info: e.json});
+      }
+    })(this);
+
+    (async (_This) => {
+      try {
+
+      const result = await rpc.get_account('taurus');
+
+      console.dir(result);
+      _This.setState({info1: result.core_liquid_balance});
+
+      } catch (e) {
+        console.log('\nCaught exception: ' + e);
+        if (e instanceof RpcError)
+          console.log(JSON.stringify(e.json, null, 2));
+        _This.setState({info1: e.json});
+      }
+    })(this);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{ JSON.stringify(this.state.info) || 'Null'}</Text>
+        <Text style={styles.instructions}>{ JSON.stringify(this.state.info1) || 'Null'}</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 /** @format */
+// index.ios.js or index.android.js
+// make sure you use `import` and not require!  
+import './shim.js'
+import crypto from 'crypto'
+// ...the rest of your code
 
 import {AppRegistry} from 'react-native';
 import App from './App';
